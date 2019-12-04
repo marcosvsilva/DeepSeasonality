0eb0272ac3aa8cfb1985da810eefedf4985d17e1
Marcos Vinicius <marcos.v.silva@live.com>
2019-12-03 21:31:55 -0300

    Alter main project

--- deep_seasonality/deep_seasonality.py
+++ deep_seasonality/deep_seasonality.py
@@ -4,27 +4,28 @@ from _log import generate_log
 
 
 def generate_directories():
-    general_path = "C:\\Jave\\Seasonality"
-    sqls_path = "{}\\sqls".format(general_path)
-    dataset_path = "{}\\dataset".format(general_path)
-
-    key_file = "{}\\key\\key.token".format(general_path)
-    config_file = "{}\\config\\config.cfg".format(general_path)
+    general_path = r"C:\Jave\Seasonality"
+    sqls_path = r"{}\sqls".format(general_path)
+    dataset_path = r"{}\dataset".format(general_path)
+    key_file = r"{}\key\key.token".format(general_path)
 
     today = date.today().strftime("%d%b%Y")
-    log_file = "{}\\logs\\log\\log_{}.log".format(general_path, today)
-    log_fail_file = "{}\\logs\\log_fail\\log_fail_{}.log".format(general_path, today)
+    log_file = r"{}\logs\log\log_{}.log".format(general_path, today)
+    log_fail_file = r"{}\logs\log_fail\log_fail_{}.log".format(general_path, today)
 
-    return {'sqls_path': sqls_path, 'dataset_path': dataset_path, 'key_file': key_file, 'config_file': config_file,
-            'log_file': log_file, 'log_fail_file': log_fail_file}
+    return {'sqls_path': sqls_path, 'dataset_path': dataset_path, 'key_file': key_file, 'log_file': log_file,
+            'log_fail_file': log_fail_file}
 
 
 def main():
+    directories = generate_directories()
     try:
-        process = Processing(generate_directories())
+        generate_log(directories['log_file'], 'Start process!')
+        process = Processing(directories)
         process.process()
+        generate_log(directories['log_file'], 'Finish process!')
     except Exception as fail:
-        generate_log(generate_directories['log_fail_file'], 'Except Interrupt System! fail: {}'.format(fail))
+        generate_log(directories['log_fail_file'], 'Except Interrupt System! fail: {}'.format(fail))
 
 
 if __name__ == '__main__':
